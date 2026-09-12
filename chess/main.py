ac7c043789e7f21601a82079113a95ac0d26ec6f
percy56-commits <warre.persijn1@gmail.com>
2026-09-12 14:54:16 +0200

    Update main.py

--- chess/main.py
+++ chess/main.py
@@ -1,6 +1,10 @@
 #import modules
 import pygame
-from test import Board
+from tempor import Board
+from movegen import get_legal_moves
+from thisthinks import minmax_fast
+from thisthinks import minmax
+from openings import openings, convert
 #initialize game
 pygame.init()
 
@@ -20,14 +24,14 @@ def make_a_move(turn):
     not_moved = True
     playing=0
     start=64
-    legal_moves = game.get_legal_moves(turn)
+    legal_moves = get_legal_moves(game.pos,turn)
     tmp = 0
-    for key in legal_moves:
-        tmp = tmp | legal_moves[key]
-    other_move= game.get_legal_moves((turn+1)%2)
+    for move in legal_moves:
+        tmp = tmp | move[1]
+    other_move= get_legal_moves(game.pos,(turn+1)%2)
     pos_moves=0
-    for key1 in other_move:
-        pos_moves = pos_moves | other_move[key1]
+    for move in other_move:
+        pos_moves = pos_moves | move[1]
     if tmp == 0 and game.pos[10+turn] & pos_moves != 0:
         print('mate')
         playing = 'mate'
@@ -42,14 +46,13 @@ def make_a_move(turn):
                     square = int(pos[0]/100)+int(pos[1]/100)*8
                     if 1<<square & game.pos[12+turn] !=0:
                         start=square
-                    elif start !=square and start <64:
+                    elif start != square and start <64:
                         end = square
-                        legal_move = legal_moves[start]
-                        if legal_move & 1<<end == 0:
-                            print('This move is not legal')
-                        else:
+                        if (start,1<<end) in legal_moves:
                             game.move(start,end)
                             not_moved = False
+                        else:
+                            print("This move is not legal")
     return playing
 
 
@@ -67,23 +70,29 @@ while running:
     play = make_a_move(turn%2)
     turn +=1
     game.draw_pieces(window)
-    
-    
     if play == "mate":
         print("checkmate")
         running = False
+        game.print_board()
     if play =='stalemate':
         print('stalemate')
         running = False
-    eval, start, end = game.minmax_fast(turn%2,4)
-    print(eval, start, end )
+        game.print_board()
+    tmp = convert(game.pos)
+    if tmp in openings:
+        start,end = openings[tmp]
+        eval = 0
+    else:
+        eval, start, end = minmax(game.pos,turn%2,3)
+    print(eval, start, end)
     if start == 0 and end == 0:
-        print("checkmate, you won")
+        print("checkmate, white won")
         running = False
+        game.print_board()
     else:
         game.move(start, end)
     turn +=1
     game.draw_pieces(window)
-print('game over')
+    print(turn)
     
-    
+game.print_board()
